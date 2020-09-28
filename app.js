bf681914a6735f88a873f8cfad5afcd017b41ec8
david manly <manly.david9@gmail.com>
2020-09-27 22:07:51 -0500

    week 4 first lesson-bin

--- app.js
+++ app.js
@@ -43,6 +43,14 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+      console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
+      res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
+  }
+});
 
 
 const campsiteRouter = require('./routes/campsiteRouter');
