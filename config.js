0a3765793cf39e7aa8fd25229ed84c6d1d596e11
david manly <manly.david9@gmail.com>
2020-09-28 22:45:04 -0500

    wekk 4 assignments- user authentication and Auth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '3951832108165944',
+        clientSecret: 'de1bfb287a84a6e3c8abe7e8c20b1c5a'
+    }
 }
